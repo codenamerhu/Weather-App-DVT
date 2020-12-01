eae02fe39b4ff5aceead6da9037f70136d2480fa
codenamerhu <rndhlovu@meetdchef.com>
2020-12-01 18:01:35 +0200

    Initial Commit

---
+++ Weather App-DVT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather App-DVT
+//
+//  Created by Jerry Boyd PTY on 2020/12/01.
+//  Copyright © 2020 codenamerhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Weather App-DVTTests/Weather_App_DVTTests.swift
@@ -0,0 +1,34 @@
+//
+//  Weather_App_DVTTests.swift
+//  Weather App-DVTTests
+//
+//  Created by Jerry Boyd PTY on 2020/12/01.
+//  Copyright © 2020 codenamerhu. All rights reserved.
+//
+
+import XCTest
+@testable import Weather_App_DVT
+
+class Weather_App_DVTTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
