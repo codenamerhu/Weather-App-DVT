f7eb57c2d2cbb795a057d2847a3496d80ba4787c
codenamerhu <rndhlovu@meetdchef.com>
2020-12-01 23:34:56 +0200

    interface builder - today weather

--- Weather App-DVT/Views/ViewController.swift
+++ Weather App-DVT/Views/ViewController.swift
@@ -20,7 +20,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
         locationManager.startMonitoringSignificantLocationChanges()
