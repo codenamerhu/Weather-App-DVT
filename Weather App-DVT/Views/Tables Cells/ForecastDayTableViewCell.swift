0aacbb94079963956acc8dd5abc21efe08932c63
codenamerhu <rndhlovu@meetdchef.com>
2020-12-02 07:32:50 +0200

    Forecast table cell

---
+++ Weather App-DVT/Views/Tables Cells/ForecastDayTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ForecastDayTableViewCell.swift
+//  Weather App-DVT
+//
+//  Created by Jerry Boyd PTY on 2020/12/02.
+//  Copyright © 2020 codenamerhu. All rights reserved.
+//
+
+import UIKit
+
+class ForecastDayTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var day: UILabel!
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var taperature: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
