3dca5f2b8646c68d351d100ee2589992ba9b0d80
codenamerhu <rndhlovu@meetdchef.com>
2020-12-02 08:01:10 +0200

    Forecast Model View

--- Weather App-DVT/Models/ModelView/ForecastWeatherViewModel.swift
+++ Weather App-DVT/Models/ModelView/ForecastWeatherViewModel.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import UIKit
+
+struct  ForecastWeatherViewModel {
+    var weekday: String?
+    var temperature: String?
+    var weatherCondition: String?
+    
+    init?(model: ForecastWeather) {
+        self.weekday = ForecastWeatherViewModel.getDayOfWeek(from: model.date)
+        self.weatherCondition = model.weatherCondition
+        self.temperature = TodayWeatherViewModel.formatValue(value: model.temperature, endStringWith: "°")
+    }
+    
+    static func getDayOfWeek(from fromDate: Double) -> String {
+        let date = Date(timeIntervalSince1970: fromDate)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        let dayOfWeekString = dateFormatter.string(from: date)
+        
+        return dayOfWeekString
+    }
+}
