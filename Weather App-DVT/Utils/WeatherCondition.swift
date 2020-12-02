64e724e4b074f110c0e0795dd682d1e03d597b6d
codenamerhu <rndhlovu@meetdchef.com>
2020-12-02 17:12:37 +0200

    weeather condition enum

---
+++ Weather App-DVT/Utils/WeatherCondition.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherCondition.swift
+//  Weather App-DVT
+//
+//  Created by Jerry Boyd PTY on 2020/12/02.
+//  Copyright © 2020 codenamerhu. All rights reserved.
+//
+
+import Foundation
+
+enum WeatherCondition {
+    case clouds
+    case sunny
+    case clear
+    case rain
+}
